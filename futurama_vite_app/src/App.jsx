33ff4283a6d4210bcba88977381087355b7b8b57
Tyler Conwell <wgc14@txstate.edu>
2025-12-03 13:41:42 -0600

    making the winner function

--- futurama_vite_app/src/App.jsx
+++ futurama_vite_app/src/App.jsx
@@ -140,6 +140,29 @@ function Board() {
 // store game state in board, not square what to display by passing prop
 
 
+// making the winner funciton for tic tac toe
+// with the squares array as input, check all winning combinations
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
 
 // -----------futurama newspaper -----------------
 
